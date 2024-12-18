c87fe4e76b20709e172b719f5e9756e42d22bbf2
c-hano <wdq0304@naver.com>
2024-12-18 12:15:46 +0900

    test auto made mathod by JPA fineBySubjectLike

--- src/main/java/Jar/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/Jar/com/mysite/sbb/QuestionRepository.java
@@ -2,6 +2,8 @@ package Jar.com.mysite.sbb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     //생성한 QuestionRepository 인터페이스를 리포지토리로 만들기 위해 JpaRepository를 상속한다.
     //JpaRepository는 JPA가 제공하는 인터페이스중 하나로 CRUD작업을 처리하는 메서드들을 이미 내장하고 있어 데이터
@@ -11,6 +13,8 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
 
     Question findBySubjectAndContent(String subjecty, String content);
+
+    List<Question> findBySubjectLike(String subject);
 
 }
 
