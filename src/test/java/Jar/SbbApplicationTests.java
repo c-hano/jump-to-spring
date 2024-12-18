84e9153ce8793b03a706e01e409967f0ad46cba3
c-hano <wdq0304@naver.com>
2024-12-18 10:32:19 +0900

    get List and asserEquals by test

--- src/test/java/Jar/SbbApplicationTests.java
+++ src/test/java/Jar/SbbApplicationTests.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -16,17 +19,12 @@ class SbbApplicationTests {
 
 	@Test
 	public void testJpa() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);
 
-		Question q2 = new Question();
-		q1.setSubject("스프링부트 모델 질문입니다.");
-		q1.setContent("id는 자동으로 생성되나요?");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(2, all.size());
+
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
 
 }
