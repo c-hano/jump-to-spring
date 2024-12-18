6428c271e9535680f5ff2f77a0b9858825a23d07
c-hano <wdq0304@naver.com>
2024-12-18 12:34:51 +0900

    change subject

--- src/test/java/Jar/SbbApplicationTests.java
+++ src/test/java/Jar/SbbApplicationTests.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -20,10 +21,11 @@ class SbbApplicationTests {
 
 	@Test
 	public void testJpa() {
-		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
-		Question q = qList.get(0);
-		assertEquals("sbb가 무엇인가요?", q.getSubject());
-
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		q.setSubject("수정된 제목");
+		this.questionRepository.save(q);
 	}
 
 
