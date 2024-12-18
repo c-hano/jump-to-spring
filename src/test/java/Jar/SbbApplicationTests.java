c3633a2569d7f7ea7d790253b776faa644730cd7
c-hano <wdq0304@naver.com>
2024-12-18 14:28:21 +0900

    get answer from question

--- src/test/java/Jar/SbbApplicationTests.java
+++ src/test/java/Jar/SbbApplicationTests.java
@@ -27,10 +27,18 @@ class SbbApplicationTests {
 	@Test
 	public void testJpa() {
 
-		Optional<Answer> oa = this.answerRepository.findById(1);
-		assertTrue(oa.isPresent());
-		Answer a = oa.get();
-		assertEquals(2, a.getQuestion().getId());
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		List<Answer> answerList = q.getAnswerList();
+
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+
+
+
+
 
 	}
 
