6a6f7d0e666a7fd792d583b58282a75e2f6e4614
c-hano <wdq0304@naver.com>
2024-12-18 09:42:00 +0900

    set JPA Repository

---
+++ src/main/java/Jar/com/mysite/sbb/QuestionRepository.java
@@ -0,0 +1,11 @@
+package Jar.com.mysite.sbb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    //생성한 QuestionRepository 인터페이스를 리포지토리로 만들기 위해 JpaRepository를 상속한다.
+    //JpaRepository는 JPA가 제공하는 인터페이스중 하나로 CRUD작업을 처리하는 메서드들을 이미 내장하고 있어 데이터
+    //관리 작업을 좀 더 편리하게 처리할 수 있다.
+    //JpaRepository<Question, Integer>은 Question 엔티티로 리포지터리를 생성한다는 의미이다.
+    //Question 엔티티의 기본키가 Integer임을 이와 같이 추가로 지정해야한다.
+}
