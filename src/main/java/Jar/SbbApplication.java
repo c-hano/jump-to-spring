a8632e3599eb1cbe0458ee5e79998ff5d4d41f42
c-hano <wdq0304@naver.com>
2024-12-18 10:29:02 +0900

    get test

--- src/main/java/Jar/SbbApplication.java
+++ src/main/java/Jar/SbbApplication.java
@@ -1,10 +1,17 @@
 package Jar;
 
+import Jar.com.mysite.sbb.Question;
+import Jar.com.mysite.sbb.QuestionRepository;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class SbbApplication {
+
     public static void main(String[] args) {
 
         SpringApplication.run(SbbApplication.class, args);
--- src/test/java/Jar/SbbApplicationTests.java
+++ src/test/java/Jar/SbbApplicationTests.java
@@ -1,13 +1,32 @@
 package Jar;
 
+import Jar.com.mysite.sbb.Question;
+import Jar.com.mysite.sbb.QuestionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class SbbApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
 	@Test
-	void contextLoads() {
+	public void testJpa() {
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);
+
+		Question q2 = new Question();
+		q1.setSubject("스프링부트 모델 질문입니다.");
+		q1.setContent("id는 자동으로 생성되나요?");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2);
 	}
 
 }
