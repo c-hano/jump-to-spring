1258705870e8c61c8264482cbe060f2fd12244b9
c-hano <wdq0304@naver.com>
2024-12-19 15:18:39 +0900

    prefix

--- src/main/java/Jar/com/mysite/question/QuestionController.java
+++ src/main/java/Jar/com/mysite/question/QuestionController.java
@@ -5,10 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+@RequestMapping("/question")
+//반복되는 프리픽스는 @RequestMapping()을 통해 괄호안에 선언하여 후에 맵핑에서 생략할 수 있다.
 @RequiredArgsConstructor
 //Lombok이넹?
 @Controller
@@ -16,14 +19,18 @@ public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
+    //@RequestMapping("/question") 을 클래스 위에 선언함으로써 해당 에노테이션은
+    //@GetMapping("/question/list") 와 같다.
     public String list(Model model) {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
 
-    @GetMapping(value = "/question/detail/{id}")
+    @GetMapping(value = "/detail/{id}")
+    //@RequestMapping("/question") 을 클래스 위에 선언함으로써 해당 에노테이션은
+    //@GetMapping("/question/detail/{id}") 와 같다.
     public String detail(Model model, @PathVariable("id") Integer id) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
