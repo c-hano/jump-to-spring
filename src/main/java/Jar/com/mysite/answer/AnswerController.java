ab38c4a46bc3b587a2eee1f584b51571e566c56a
c-hano <wdq0304@naver.com>
2024-12-20 16:07:36 +0900

    get answerController

---
+++ src/main/java/Jar/com/mysite/answer/AnswerController.java
@@ -0,0 +1,11 @@
+package Jar.com.mysite.answer;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+public class AnswerController {
+
+}
