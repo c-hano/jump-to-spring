a157456a4034c1359ea479d653bec8086e52365e
c-hano <wdq0304@naver.com>
2024-12-24 14:51:00 +0900

    get PostMapping QuestionController

--- src/main/java/Jar/com/mysite/question/QuestionController.java
+++ src/main/java/Jar/com/mysite/question/QuestionController.java
@@ -3,10 +3,7 @@ package Jar.com.mysite.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,8 +34,15 @@ public class QuestionController {
         return "question_detail";
     }
 
-    @GetMapping(value = "/create")
-    public String questionCreate() {
+    @GetMapping(value="/create")
+    public String questionCreate(){
         return "question_form";
+    }
+
+    @PostMapping(value = "/create")
+    public String questionCreate(@RequestParam(value = "subject") String subject,
+                                 @RequestParam(value = "content") String content) {
+        //TODO 질문을 저장한다.
+        return "redirect:/question/list"; // 질문 저장 후 질문 목록으로 이동
     }
 }
